535d73ccac5ca6fd227f22d731efb058dd2b2d4f
avkoll <avkoller01@hotmail.com>
2024-09-06 15:36:14 -0400

    added token

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-import sqlite3
 
-# Load the text-to-SQL model
 model_name = "Salesforce/wikisql"
+token = "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"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-# Convert natural language to SQL
-query = "Show me the names of employees who earn more than 5000"
-inputs = tokenizer.encode(query, return_tensors="pt")
-outputs = model.generate(inputs)
-sql_query = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-print("Generated SQL:", sql_query)
+def natural_language_to_sql(query):
+    inputs = tokenizer.encode(query, return_tensors="pt")
+    outputs = model.generate(inputs)
+    sql_query = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return sql_query
 
-# Set up a database connection
-conn = sqlite3.connect('your_database.db')
-cursor = conn.cursor()
 
-# Execute the generated SQL
-cursor.execute(sql_query)
-rows = cursor.fetchall()
-
-for row in rows:
-    print(row)
-
-conn.close()
+nl_query = "Show me the names of employees who earn more than 5000"
+sql_query = natural_language_to_sql(nl_query)
+print("Generated SQL Query:", sql_query)
