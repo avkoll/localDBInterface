697d846709f3e3b613295737677945c93f90ec45
avkoll <avkoller01@hotmail.com>
2024-09-06 23:36:51 -0400

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-The goal of this project is to create a NL2SQL LLM that 
-
+The goal of this project is to create a NL2SQL LLM that can talk to a sql database. It will elminate the need to learn to write in SQL. 
+This will of course be more complicated than I think, but I want this to follow \  
+![Architecture](static/Architecture%202.png)
 
 CUDA 11.8
 
@@ -7,3 +8,8 @@ CUDA 11.8
 ### tscholak/cxmefzzi
 This model is the T5-3B trained on the spider database and is fin tuned to be used something called PICARD \
 I want to use this model and fine tune it on whatever database I will be 'talking' to. 
+
+### meta-llama/Meta-Llama-3.1-8B-Instruct
+This is llama, a bunch of things I read said this was one of the best to run locally and can run on a consumer pc \
+I want to use the larger models but I only have a 2070 and don't think I would be able to run them. I need to save for a new pc \
+das
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # Load the pre-trained model and tokenizer from Hugging Face
-model_name = "tscholak/cxmefzzi"
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+model = AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 
@@ -22,12 +22,15 @@ def natural_language_to_sql(query, model, tokenizer, max_length=128):
     try:
         # Tokenize the input query
         inputs = tokenizer.encode(query, return_tensors="pt", truncation=True, max_length=max_length)
+        print(f"Tokenized input: {inputs}")
 
         # Generate the SQL query using the model
-        outputs = model.generate(inputs, max_length=max_length, num_beams=4, early_stopping=True)
+        outputs = model.generate(inputs, max_length=max_length, num_beams=6, early_stopping=True)
+        print(f"Model raw output: {outputs}")
 
         # Decode the generated SQL query
         sql_query = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        print(f"Decoded SQL Query: {sql_query}")
 
         return sql_query
     except Exception as e:
@@ -36,7 +39,8 @@ def natural_language_to_sql(query, model, tokenizer, max_length=128):
 
 
 # Example usage
-nl_query = input("Enter a natural language query: ")
+nl_query = "Generate SQL Query for: " + input("Enter a natural language query: ")
+print(f"Input Query: {nl_query}")
 sql_query = natural_language_to_sql(nl_query, model, tokenizer)
 
 if sql_query:
